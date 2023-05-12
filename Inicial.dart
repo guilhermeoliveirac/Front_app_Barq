dd338a4fc0194a88cdaf855c6f2d7b18008cd5fb
Francielle Lustosa <86414886+FranLustosa@users.noreply.github.com>
2023-05-12 08:42:53 -0300

    Add files via upload

--- Inicial.dart
+++ Inicial.dart
@@ -30,7 +30,14 @@ class _MyStatefulWidgetState extends State<Inicial> {
               padding: const EdgeInsets.all(8.0),
               child: Align(
                 alignment: Alignment(-1, 0),
-                child: Text("         Olá! Bem vindo ao BARQ!"),
+                child: Text(
+                  "        Olá, Bem vindo ao BARQ!",
+                  style: TextStyle(
+                    color: Color(0xFF00265F),
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ),
             ),
             SizedBox(height: 10),
@@ -56,21 +63,39 @@ class _MyStatefulWidgetState extends State<Inicial> {
                 ),
               ],
             ),
-            SizedBox(height: 30),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment(-1, 0),
+                child: Text(
+                  "     Menu Categorias ",
+                  style: TextStyle(
+                    color: Color(0xFF00265F),
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Container(
                   alignment: Alignment.center,
                   child: Text(
-                    " Bebidas Alcóolicas\n",
-                    style: TextStyle(color: Colors.white),
+                    "   \n   Bebidas\n Alcoólicas\n",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
                   ),
                   width: 100,
                   height: 150,
                   decoration: BoxDecoration(
                       color: Color(0xFF00265F),
-                      borderRadius: BorderRadius.circular(15),
+                      borderRadius: BorderRadius.circular(10),
                       image: DecorationImage(
                           alignment: Alignment(0, 0),
                           image: AssetImage("assets/images/card2.png"),
@@ -79,37 +104,99 @@ class _MyStatefulWidgetState extends State<Inicial> {
                 Container(
                   alignment: Alignment(-1, 0),
                   child: Text(
-                    " Bebidas Alcóolicas\n",
-                    style: TextStyle(color: Colors.white),
+                    "   \n      Bebidas\n          não \n    Alcoólicas\n",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
                   ),
                   width: 100,
                   height: 150,
                   decoration: BoxDecoration(
                       color: Color(0xFF00265F),
-                      borderRadius: BorderRadius.circular(15),
+                      borderRadius: BorderRadius.circular(10),
                       image: DecorationImage(
                           alignment: Alignment(0, 0),
-                          image: AssetImage("assets/images/card2.png"),
+                          image: AssetImage("assets/images/card3.png"),
                           fit: BoxFit.fitHeight)),
                 ),
                 Container(
                   alignment: Alignment(-1, 0),
                   child: Text(
-                    " Bebidas Alcóolicas\n",
-                    style: TextStyle(color: Colors.white),
+                    "     Petiscos",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
                   ),
                   width: 100,
                   height: 150,
                   decoration: BoxDecoration(
                       color: Color(0xFF00265F),
-                      borderRadius: BorderRadius.circular(15),
+                      borderRadius: BorderRadius.circular(10),
                       image: DecorationImage(
                           alignment: Alignment(0, 0),
-                          image: AssetImage("assets/images/card2.png"),
+                          image: AssetImage("assets/images/card4.png"),
                           fit: BoxFit.fitHeight)),
                 )
               ],
             ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment(-1, 0),
+                child: Text(
+                  "     Menu Categorias ",
+                  style: TextStyle(
+                    color: Color(0xFF00265F),
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment(-1, 0),
+              child: Text(
+                "   Dobradinha de Chopp\n",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              width: 350,
+              height: 90,
+              decoration: BoxDecoration(
+                  color: Color(0xFF00265F),
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                      alignment: Alignment(1, 0),
+                      image: AssetImage("assets/images/card5.png"),
+                      fit: BoxFit.fitHeight)),
+            ),
+            SizedBox(height: 20),
+            Container(
+              alignment: Alignment(-1, 0),
+              child: Text(
+                "   Dobradinha de Chopp\n",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              width: 350,
+              height: 90,
+              decoration: BoxDecoration(
+                  color: Color(0xFF00265F),
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                      alignment: Alignment(1, 0),
+                      image: AssetImage("assets/images/card5.png"),
+                      fit: BoxFit.fitHeight)),
+            )
           ],
         ),
       ),
